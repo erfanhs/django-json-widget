d10ca5a2487104e013efb34c7009728bb780aeab
Erfan Harirsaz <erfanharirsaz071@gmail.com>
2023-10-03 00:47:50 -0700

    Update widgets.py

--- django_json_widget/widgets.py
+++ django_json_widget/widgets.py
@@ -20,7 +20,7 @@ class JSONEditorWidget(forms.Widget):
 
     def __init__(self, attrs=None, mode='view', options=None, width=None, height=None):
         default_options = {
-            'modes': ['view'],
+            'modes': ['view', 'text'],
             'mode': mode,
             'search': True,
         }
